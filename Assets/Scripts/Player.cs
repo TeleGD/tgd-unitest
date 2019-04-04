a0ec4f8b2e72dd06871159f3b546040517087510
Christopher SCHIRLE <christopher.schirle@telecomnancy.eu>
2019-04-04 15:03:59 +0200

    Transfert des données du gitlab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,27 +5,37 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
+    private float Hspeed;
+    [SerializeField]
+    private float Vspeed;
 
     [SerializeField]
     private bool isDead;
 
     [SerializeField]
     private Rigidbody2D rb;
+    public GameObject hitup;
+    public GameObject hitdown;
 
-    Vector2 moveDirection;
+    [SerializeField]
+    private bool triggerGround;
+
+    private Vector2 moveDirection;
+    private float moveVertical;
 
 
     // Start is called before the first frame update
     void Start()
     {
         isDead = false;
+        triggerGround = false;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 
     void FixedUpdate(){
@@ -33,12 +43,27 @@ public class Player : MonoBehaviour
         float moveHorizontal = Input.GetAxis ("Horizontal");
 
         // Stocke l'entrée verticale dans la variable moveVertical.
-        float moveVertical = Input.GetAxis ("Vertical");
+        if (triggerGround && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(new Vector2(0, 1 * Vspeed),ForceMode2D.Impulse);
+            triggerGround = false;
+        }
+
 
         // Utilise les deux nombres pour créer un Vector2.
-        moveDirection = new Vector2 (moveHorizontal, moveVertical);
+        moveDirection = new Vector2 (moveHorizontal*Hspeed, moveVertical*Vspeed);
 
         // Appelle la fonction AddForce() du Rigidbody2D rb avec le mouvement multiplié par la vitesse.
-        rb.AddForce (moveDirection * speed);
+        rb.AddForce(moveDirection);
+    }
+
+    public void setTriggerGround(bool b)
+    {
+        triggerGround = b;
+    }
+
+    public bool getTriggerGround()
+    {
+        return triggerGround;
     }
 }
